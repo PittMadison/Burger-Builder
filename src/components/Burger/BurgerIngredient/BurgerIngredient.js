6a011b4f02f1a4c0dda1fd9d3495eecf5111bce4
Pitt Madison <karimovpg@gmail.com>
2019-06-13 19:15:25 +0300

    burger composition added with media q

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -15,11 +15,14 @@ class BurgerIngredient extends Component {
             break;
         case ('bread-top'):
             ingredient = (
-                <div className={classes.BreadBottom}>
+                <div className={classes.BreadTop}>
                 <div className={classes.Seeds1}></div>
                 <div className={classes.Seeds2}></div>
                 </div>
                 );
+            break;
+        case ('meat'):
+            ingredient = <div className={classes.Meat}></div>;
             break;
         case ('cheese'):
             ingredient = <div className={classes.Cheese}></div>;
