8bdeecf023d34ca15150043ee505c28eda6fe308
Pitt Madison <karimovpg@gmail.com>
2019-06-14 00:32:28 +0300

    added controls and more/less logic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
-
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,16 +17,41 @@ class BurgerBuilder extends Component {
             salad: 1,
             cheese: 2,
             meat: 2
-        }
+        },
+        totalPrice: 4
     }
 
-    render() {
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceSubstraction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceSubstraction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    render() { 
         return (
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>
-                    Build Controls
-                </div>
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Auxiliary>
         );
     }
