853e8bb10d2ee1f1f817f41cff6ee0a5b645ef3a
Pitt Madison <karimovpg@gmail.com>
2019-06-15 11:57:36 +0300

    menu button changed

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,8 +3,10 @@ import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => {
     return (
-        <div onClick={props.clicked}>
-           MENU 
+        <div className={classes.DrawerToggle} onClick={props.clicked}>
+           <div></div>
+           <div></div>
+           <div></div>
         </div>
     )
 }
