9dc52a74466a2f71c474e8ba2d3c2d62480ca8ce
Pitt Madison <karimovpg@gmail.com>
2019-06-15 11:50:32 +0300

    sidedrawer open/close logics added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => (
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    render () {
+
+        return (
+
     <Auxiliary>
-    <Toolbar/>
-    <SideDrawer/>
+        <Toolbar/>
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
         <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
         </main>
     </Auxiliary>
-    );
+        )
+    }
+    
+};
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,27 @@ import React from 'react'
 import classes from './SIdeDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../../UI/Backdrop/Backdrop';
+import Modal from '../../../UI/Modal/Modal';
+import Auxiliary from '../../../hoc/Auxiliary';
+
 
 const sideDrawer = (props) => {
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo/>
+        <Auxiliary>
+            <Modal/>
+            <div className={props.open ?
+            [classes.SideDrawer, classes.Open].join(' ') :
+            [classes.SideDrawer, classes.Close].join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-            
-        </div>
+            {props.open && <Backdrop hide={props.closed}/>}
+        </Auxiliary>
     );
 };
 
